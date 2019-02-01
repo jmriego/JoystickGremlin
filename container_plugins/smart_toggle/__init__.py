737c7f91f2644c6d9673ceca0bd1f9708da31484
jmriego <jmriego@telefonica.net>
2019-02-01 08:22:54 +0000

    first commit for smart toggle

---
+++ container_plugins/smart_toggle/__init__.py
@@ -0,0 +1,263 @@
+# -*- coding: utf-8; -*-
+
+# Copyright (C) 2015 - 2018 Lionel Ott
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import copy
+import logging
+import threading
+import time
+from xml.etree import ElementTree
+
+from PyQt5 import QtWidgets
+
+import gremlin
+import gremlin.ui.common
+import gremlin.ui.input_item
+
+
+class SmartToggleContainerWidget(gremlin.ui.input_item.AbstractContainerWidget):
+
+    """SmartToggle container which holds a single action."""
+
+    def __init__(self, profile_data, parent=None):
+        """Creates a new instance.
+
+        :param profile_data the profile data represented by this widget
+        :param parent the parent of this widget
+        """
+        super().__init__(profile_data, parent)
+
+    def _create_action_ui(self):
+        """Creates the UI components."""
+        self.profile_data.create_or_delete_virtual_button()
+
+        self.options_layout = QtWidgets.QHBoxLayout()
+
+        # Activation delay
+        self.options_layout.addWidget(
+            QtWidgets.QLabel("<b>Long press delay: </b>")
+        )
+        self.delay_input = gremlin.ui.common.DynamicDoubleSpinBox()
+        self.delay_input.setRange(0.1, 2.0)
+        self.delay_input.setSingleStep(0.1)
+        self.delay_input.setValue(0.5)
+        self.delay_input.setValue(self.profile_data.delay)
+        self.delay_input.valueChanged.connect(self._delay_changed_cb)
+        self.options_layout.addWidget(self.delay_input)
+        self.options_layout.addStretch()
+
+        # Toggle type (hold / momentary)
+        self.options_layout.addWidget(QtWidgets.QLabel("<b>Toggle type:</b>"))
+        self.toggle_hold = QtWidgets.QRadioButton("hold")
+        self.toggle_momentary = QtWidgets.QRadioButton("momentary")
+        if self.profile_data.toggle_type == "hold":
+            self.toggle_hold.setChecked(True)
+        else:
+            self.toggle_momentary.setChecked(True)
+        self.toggle_hold.toggled.connect(self._toggle_type_changed_cb)
+        self.toggle_momentary.toggled.connect(self._toggle_type_changed_cb)
+        self.options_layout.addWidget(self.toggle_hold)
+        self.options_layout.addWidget(self.toggle_momentary)
+
+        self.action_layout.addLayout(self.options_layout)
+
+        if len(self.profile_data.action_sets) > 0:
+            assert len(self.profile_data.action_sets) == 1
+
+            widget = self._create_action_set_widget(
+                self.profile_data.action_sets[0],
+                "SmartToggle",
+                gremlin.ui.common.ContainerViewTypes.Action
+            )
+            self.action_layout.addWidget(widget)
+            widget.redraw()
+            widget.model.data_changed.connect(self.container_modified.emit)
+        else:
+            action_selector = gremlin.ui.common.ActionSelector(
+                self.profile_data.get_input_type()
+            )
+            action_selector.action_added.connect(self._add_action)
+            self.action_layout.addWidget(action_selector)
+
+    def _create_condition_ui(self):
+        if len(self.profile_data.action_sets) > 0 and \
+                self.profile_data.activation_condition_type == "action":
+            assert len(self.profile_data.action_sets) == 1
+
+            widget = self._create_action_set_widget(
+                self.profile_data.action_sets[0],
+                "SmartToggle",
+                gremlin.ui.common.ContainerViewTypes.Condition
+            )
+            self.activation_condition_layout.addWidget(widget)
+            widget.redraw()
+            widget.model.data_changed.connect(self.container_modified.emit)
+
+    def _add_action(self, action_name):
+        """Adds a new action to the container.
+
+        :param action_name the name of the action to add
+        """
+        plugin_manager = gremlin.plugin_manager.ActionPlugins()
+        action_item = plugin_manager.get_class(action_name)(self.profile_data)
+        if self.profile_data.action_sets[0] is None:
+            self.profile_data.action_sets[0] = []
+        self.profile_data.action_sets[0].append(action_item)
+        self.profile_data.create_or_delete_virtual_button()
+        self.container_modified.emit()
+
+    def _delay_changed_cb(self, value):
+        """Updates the activation delay value.
+
+        :param value the value after which the long press action activates
+        """
+        self.profile_data.delay = value
+
+    def _toggle_type_changed_cb(self, value):
+        """Updates the toggle type
+
+        :param value whether or not the selection was toggled - ignored
+        """
+        if self.toggle_hold.isChecked():
+            self.profile_data.toggle_type = "hold"
+        else:
+            self.profile_data.toggle_type = "momentary"
+
+    def _handle_interaction(self, widget, action):
+        """Handles interaction icons being pressed on the individual actions.
+
+        :param widget the action widget on which an action was invoked
+        :param action the type of action being invoked
+        """
+        pass
+
+    def _get_window_title(self):
+        """Returns the title to use for this container.
+
+        :return title to use for the container
+        """
+        if len(self.profile_data.action_sets) > 0:
+            return ", ".join(a.name for a in self.profile_data.action_sets[0])
+        else:
+            return "SmartToggle"
+
+
+class SmartToggleContainerFunctor(gremlin.base_classes.AbstractFunctor):
+
+    """Executes the contents of the associated SmartToggle container."""
+
+    def __init__(self, container):
+        super().__init__(container)
+        self.action_set = gremlin.execution_graph.ActionSetExecutionGraph(
+            container.action_sets[0]
+        )
+        self.delay = container.delay
+        self.toggle_type = container.toggle_type
+        self.toggle_status = False
+        self.value_press = None
+        self.event_press = None
+
+    def process_event(self, event, value):
+        # TODO: Currently this does not handle hat or axis events, however
+        #       virtual buttons created on those inputs is supported
+        if not isinstance(value.current, bool):
+            logging.getLogger("system").warning(
+                "Invalid data type received in Tempo container: {}".format(
+                    type(event.value)
+                )
+            )
+            return False
+
+        # Copy state when input is pressed
+        if isinstance(value.current, bool) and value.current:
+            self.value_press = copy.deepcopy(value)
+            self.event_press = event.clone()
+
+        if self.toggle_type == 'hold':
+            value_toggle.current = self.toggle_status
+
+    def _process_modified_press_event(current):
+        if current is not None:
+            value = copy.deepcopy(value)
+            event = event.clone()
+            value.current = current
+            event.is_pressed = current
+            self.action_set.process_event(event, value)
+
+class SmartToggleContainer(gremlin.base_classes.AbstractContainer):
+
+    """Represents a container which holds exactly one action."""
+
+    name = "SmartToggle"
+    tag = "smart_toggle"
+
+    input_types = [
+        gremlin.common.InputType.JoystickAxis,
+        gremlin.common.InputType.JoystickButton,
+        gremlin.common.InputType.JoystickHat,
+        gremlin.common.InputType.Keyboard
+    ]
+    interaction_types = []
+
+    functor = SmartToggleContainerFunctor
+    widget = SmartToggleContainerWidget
+
+    def __init__(self, parent=None):
+        """Creates a new instance.
+
+        :param parent the InputItem this container is linked to
+        """
+        super().__init__(parent)
+        self.action_sets = [[]]
+        self.delay = 0.5
+        self.toggle_type = "hold"
+
+    def _parse_xml(self, node):
+        """Populates the container with the XML node's contents.
+
+        :param node the XML node with which to populate the container
+        """
+        super()._parse_xml(node)
+        self.delay = float(node.get("delay", 0.5))
+        self.toggle_type = node.get("toggle-type", "hold")
+
+    def _generate_xml(self):
+        """Returns an XML node representing this container's data.
+
+        :return XML node representing the data of this container
+        """
+        node = ElementTree.Element("container")
+        node.set("type", SmartToggleContainer.tag)
+        node.set("delay", str(self.delay))
+        node.set("toggle-type", str(self.toggle_type))
+        as_node = ElementTree.Element("action-set")
+        for action in self.action_sets[0]:
+            as_node.append(action.to_xml())
+        node.append(as_node)
+        return node
+
+    def _is_container_valid(self):
+        """Returns whether or not this container is configured properly.
+
+        :return True if the container is configured properly, False otherwise
+        """
+        return len(self.action_sets) == 1
+
+
+# Plugin definitions
+version = 1
+name = "smart_toggle"
+create = SmartToggleContainer
